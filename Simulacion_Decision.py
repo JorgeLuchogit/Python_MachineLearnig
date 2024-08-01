efbd93302b1422cabf9a2d416d763bfbc7f8458d
JL-Lucho Cervantes <vhlucho@ciencias.unam.mx>
2024-08-01 14:37:20 -0600

    Update Simulacion_Decision.py
    Se mejora el algoritmo.

--- Simulacion_Decision.py
+++ Simulacion_Decision.py
@@ -4,19 +4,33 @@ Programa que simula un conjunto de datos de decisión a los cuales se les ajusta
 
 from pandas import DataFrame
 from numpy.random import randint
+from math import exp
 
-
-def simulacion_desicion_binaria(num_mues):
+def simulacion_desicion_binaria(num_mues, salto=0.5):
     """
     num_muest: numero de datos que se quieren obtener.
+    salto:     Punto en donde se da el salto de los datos. Por defecto se tiene 0.5, por lo que el salto dse da a la mitad de la distribución.
     """
-    decision = randint(0,50,num_mues)
+    if salto <= 0 or salto >= 1:
+
+        print('Fraccion no valida. Coloque un número entre 0 y 1')
+        exit()
+
+    decision = randint(0,11, num_mues)
 
     for i in range(num_mues):
-        if decision[i] < i:
-            decision[i] = 1
+
+        if i < num_mues*salto:
+            if decision[i] == 1: 
+                decision[i] = 1
+            else:
+                decision[i] = 0
         else:
-            decision[i] =0
+            if decision[i] != 1 : 
+                decision[i] = 1
+            else:
+                decision[i] = 0
+
     return DataFrame(decision)
 
 
